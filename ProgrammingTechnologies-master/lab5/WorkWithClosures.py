0022f37cb5498ef9d7189ac41694cce0b0b975f5
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-12 22:39:29 +0300

    Создание WorkWithClosures.py

---
+++ ProgrammingTechnologies-master/lab5/WorkWithClosures.py
@@ -0,0 +1,39 @@
+import string
+
+from PrintUtil import printHeader, printTitleTask
+
+
+def mainWorkWithClosures():
+    printHeader("RECURSION")
+
+    printTitleTask(4)
+    print(closure_sum(1)(2))
+
+    printTitleTask(5)
+    print(closure_comparison('>')('>'))
+
+    printTitleTask(10)
+    print(closure_list_del([1, 3, 12, 14, 15, 17])(3))
+
+
+def closure_sum(number: int):
+    def inner(inner_number: int):
+        return inner_number + number
+
+    return inner
+
+
+def closure_comparison(symbol: string):
+    true_symbols = ['>', '<', '=']
+
+    def inner(inner_symbol: string):
+        return inner_symbol == symbol and symbol in true_symbols
+
+    return inner
+
+
+def closure_list_del(numbers: list):
+    def inner(n: int):
+        return [i for i in numbers if i % n != 0]
+
+    return inner
