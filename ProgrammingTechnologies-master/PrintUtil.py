f9a77cbe11d48693d2da489c0ce3370b5d8013c9
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-12 22:38:21 +0300

    Initial commit

---
+++ ProgrammingTechnologies-master/PrintUtil.py
@@ -0,0 +1,11 @@
+import string
+
+
+def printTitleTask(number: int):
+    print("\nЗадание: %d" % number)
+
+
+def printHeader(header: string):
+    print("--------------")
+    print(header)
+    print("--------------")
---
+++ ProgrammingTechnologies-master/main.py
@@ -0,0 +1,6 @@
+from lab1.WorkWithTuple import mainWorkWithTuple
+from lab2.WorkWithDict import mainWorkWithDict
+from lab2.WorkWithSet import mainWorkWithSet
+
+mainWorkWithDict()
+
