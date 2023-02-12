bccebe9c7b02113ce0ea61701b9bb42a3f209d6a
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-12 22:40:58 +0300

    Создание WorkWithString.py

---
+++ ProgrammingTechnologies-master/lab1/WorkWithString.py
@@ -0,0 +1,32 @@
+import string
+
+from PrintUtil import printHeader, printTitleTask
+
+
+def mainWorkWithString():
+    printHeader("STRINGS")
+
+    printTitleTask(4)
+    task4()
+
+    printTitleTask(5)
+    task5()
+
+    printTitleTask(10)
+    task10()
+
+def task4():
+    str1: string = "Цель работы: познакомиться с основными"
+    str1 = str1.replace("о", "*")
+    print(str1)
+
+
+def task5():
+    s1: string = input("Введите слово \n")
+    s2: string = s1[0] + s1[len(s1) // 2] + s1[-1]
+    print(s2)
+
+
+def task10():
+    s1: string = "Как дела?"
+    print(s1[::-1])
